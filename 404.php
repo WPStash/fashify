3d9d7a9e1f82987f58e163df0af5ef23472c788c
Cong Thien <luckystar.ictu90@gmail.com>
2016-04-02 16:29:58 +0700

    Update 404 page

--- 404.php
+++ 404.php
@@ -9,12 +9,14 @@
 
 get_header(); ?>
 
+<div class="container">
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'fashify' ); ?></h1>
+					<h1 class="entry-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'fashify' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
@@ -60,5 +62,7 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
+	<?php  get_sidebar(); ?>
+</div>
 <?php
 get_footer();
